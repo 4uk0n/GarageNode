4929343dc873e37d2a3499212fdf691f700b6213
Yukon <alexanderhedrick@Alexanders-MacBook-Pro.local>
2018-03-14 21:48:09 -0400

    Dev Update
    1. Added index.js
    2. Added scipt tags, with references to socket.io and index.js script files
    3. Add route to app.js for index.js and socket.io.js
    4. Tested javascript functionality

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-var express = require('express');
-var app = express();
-var http = require('http').createServer(handler);
-var router = express.Router();
-
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 //ROUTES
-// Redirects to index.html when root level requested
 app.get('/',function(req,res,next){
 res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('/index',function(req,res,next){
-res.sendFile(__dirname + '/public/index.html');  
+res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/index.js',function(req,res,next){
+res.sendFile(__dirname + '/public/index.js');
+});
+
+app.get('/socket.io.js',function(req,res,next){
+res.sendFile(__dirname + '/node_modules/socket.io-client/dist/socket.io.js');
+});
 
 app.listen(8080);
