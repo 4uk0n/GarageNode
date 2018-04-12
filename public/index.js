7fbf90609800e998321dce80997fd0683e22adf7
Yukon <alexanderhedrick@Alexanders-MacBook-Pro.local>
2018-04-11 21:44:16 -0400

    test js update

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 var num = 0;
-var socket = io()
+var socket = io.connect('http://localhost');
 var btn = document.getElementById("demo");
 
 btn.addEventListener("click", btnClick);
@@ -7,7 +7,10 @@ btn.addEventListener("click", btnClick);
 function btnClick() {
   if (num == 0){
     document.getElementById("demo").className = "btn btn-success btn-lg";
-    socket.emit("light", Number(this.checked))
+
+    socket.on('news', function (data) {
+        console.log('test');
+});
     num += 1;
   } else {
     document.getElementById("demo").className = "btn btn-primary btn-lg";
