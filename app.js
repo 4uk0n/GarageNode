dfa528535e9fea1b9288740d9c12067f6160914b
Yukon <alexanderhedrick@Alexanders-MacBook-Pro.local>
2018-04-11 21:05:51 -0400

    Major Changes
    Used to test node app.

--- app.js
+++ app.js
@@ -19,4 +19,16 @@ app.get('/socket.io.js',function(req,res,next){
 res.sendFile(__dirname + '/node_modules/socket.io-client/dist/socket.io.js');
 });
 
-app.listen(8080);
+app.listen(80, function () {
+  console.log(‘server running on port 3000’);
+})
+
+io.sockets.on('connection', function (socket) {// WebSocket Connection
+  var lightvalue = 0; //static variable for current status
+  socket.on('light', function(data) { //get light switch status from client
+    lightvalue = data;
+    if (lightvalue) {
+      console.log(lightvalue); //turn LED on or off, for now we will just show it in console.log
+    }
+  });
+});
--- public/index.js
+++ public/index.js
@@ -1,10 +1,13 @@
 var num = 0;
+var socket = io()
+var btn = document.getElementById("demo");
 
-document.getElementById("demo").addEventListener("click", btnClick);
+btn.addEventListener("click", btnClick);
 
 function btnClick() {
   if (num == 0){
     document.getElementById("demo").className = "btn btn-success btn-lg";
+    socket.emit("light", Number(this.checked))
     num += 1;
   } else {
     document.getElementById("demo").className = "btn btn-primary btn-lg";
