1446ee63f34eff81fb2bb058992cd6b37f4505e1
Yukon <alexanderhedrick@Alexanders-MacBook-Pro.local>
2018-04-11 21:45:19 -0400

    test app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+server.listen(80);
 //ROUTES
 app.get('/',function(req,res,next){
 res.sendFile(__dirname + '/public/index.html');
@@ -19,16 +20,13 @@ app.get('/socket.io.js',function(req,res,next){
 res.sendFile(__dirname + '/node_modules/socket.io-client/dist/socket.io.js');
 });
 
-app.listen(80, function () {
-  console.log(‘server running on port 3000’);
-})
+// app.listen(80, function () {
+//   console.log(‘server running on port 3000’);
+// })
 
-io.sockets.on('connection', function (socket) {// WebSocket Connection
-  var lightvalue = 0; //static variable for current status
-  socket.on('light', function(data) { //get light switch status from client
-    lightvalue = data;
-    if (lightvalue) {
-      console.log(lightvalue); //turn LED on or off, for now we will just show it in console.log
-    }
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
   });
 });
